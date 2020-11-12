e14dc6bdf188423aa2067335c34aee9f471df693
Napo <Michaeljnapolitano@gmail.com>
2020-11-12 15:40:04 -0500

    add logout after 1 hour

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
-const path = require('path');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 
-const session = require('express-session');
+const session = require("express-session");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: process.env.DB_SECRET,
-  cookie: {},
+  cookie: {
+    expires: 60000000,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
 
 // turn on routes
@@ -36,5 +38,5 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
